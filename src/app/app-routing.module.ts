83b2dec450a9291c0f44700c1e4d3b25db064fd7
Milan_Miljevic <mojemail@nesto.com>
2021-09-17 12:17:00 +0200

    invoices-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvoicesPageComponent } from './ui/invoices-page/invoices-page.component';
+import { LoginPageComponent } from './ui/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'invoices', component: InvoicesPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
