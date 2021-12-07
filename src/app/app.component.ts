5d9faaa7d4e5152199e95cb3e8ffdf035d34b4f2
MiljevicM <milan191090@gmail.com>
2021-12-07 14:18:29 +0100

    title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stockapp';
+
+  constructor(private router: Router, private _titleService: Title) {}
+
+  ngOnInit() {
+  
+    this.changeTitle();
+    
+  }
+  
+  changeTitle() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof ActivationStart) {
+        let data = event.snapshot.data;
+        if (data["title"]) {
+          this._titleService.setTitle("Digit - " + data["title"]);
+        } else {
+          this._titleService.setTitle("Digit");
+        }
+      }
+    });
+  }
 
 }
 
